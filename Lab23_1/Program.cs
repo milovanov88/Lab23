dda54e08f0ba3978e3efae5c03af877ca3a395da
milov <milov@DESKTOP-M98P6EU>
2022-01-07 16:22:19 +0300

    Добавьте файлы проекта.

---
+++ Lab23_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab23_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача №23");
+            Console.WriteLine("Задайте число для нахождения факториала");
+            int n = Convert.ToInt32(Console.ReadLine());
+            actorialAsync(n+1);
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadLine();
+        }
+        static void factorial(int n)
+        {
+            int s = 1;
+                for (int i = 1; i < n; i++)
+            {
+                s *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(s);
+        }
+        static async void actorialAsync(int n)
+        {
+            await Task.Run(() => factorial(n));
+        }
+    }
+}
